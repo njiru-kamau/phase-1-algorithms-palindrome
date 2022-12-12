3238abcef526983d6c51549f68a4ad541f9386ab
njiru-kamau <maureenkamau947@gmail.com>
2022-12-12 13:31:06 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,13 +1,32 @@
 function isPalindrome(word) {
   // Write your algorithm here
+
+  var str = str.toLowerCase();
+  var str = str.replace(/[^a-z]/g, '');
+  var str = str.split('');
+  var str = str.reverse();
+  var str = str.join('');
+  if (str === str) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 /* 
   Add your pseudocode here
-*/
+//a Palindrome takes a string as input.
+
+create a variable  that is equal to an empty string.
+
+To iterate over the input string in reverse order, use a loop. Add one character to the beginning of the "reversedString" variable for each character in the input string.
+
+After the loop has finished, check to see if the "reversedString" variable equals the input string. Return "true" if the input string is a palindrome. Return "false" if it is not a palindrome.
+
 
 /*
   Add written explanation of your solution here
+
 */
 
 // You can run `node index.js` to view these console logs
